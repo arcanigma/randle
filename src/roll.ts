4e18b0c73feb21edb8552b5db2493272df88ef35
Ron Reaser <ron@reaser.org>
2020-09-19 16:12:08 -0400

    Remove support for () and [] groups

--- src/roll.ts
+++ src/roll.ts
@@ -1,4 +1,4 @@
-import { App, Context, MessageEvent, Middleware, SlackEventMiddlewareArgs } from '@slack/bolt';
+import { App, MessageEvent, Middleware, SlackEventMiddlewareArgs } from '@slack/bolt';
 import { Block, ContextBlock, MrkdwnElement, SectionBlock } from '@slack/web-api';
 import { MongoClient } from 'mongodb';
 import ordinal from 'ordinal';
@@ -9,71 +9,19 @@ import { anywhere } from './library/listeners';
 import { blame } from './library/messages';
 
 export const events = (app: App, store: Promise<MongoClient>): void => {
-    type Clause = {
-        text: string;
-        where: string;
-    };
-
     // TODO refactor into parser
-    // TODO support JSON Script embeds, even in macros
 
-    const re_roll = /^!?roll\s+(.+)/i,
-        re_parens = /(?:\([^'()][^()]*\)|\[[^'[\]][^[\]]*\])/g;
-    const listen_roll: Middleware<SlackEventMiddlewareArgs<'message'>> = async ({ message, context, next }) => {
-        if (message.text && !message.subtype) {
-            let matches;
-            if ((matches = re_roll.exec(wss(message.text)))) {
-                context.clauses = [{
-                    text: matches[1],
-                    where: 'inline'
-                }];
-                await next?.();
-            }
-            else if ((matches = wss(message.text).match(re_parens))) {
-                context.clauses = matches.map(m => ({
-                    text: m.slice(1,-1),
-                    where: m.slice(0, 1) == '[' && !message.thread_ts ? 'thread' : 'inline'
-                }));
-                await next?.();
-            }
-        }
+    const re_semi = /\s*;\s*/;
+    const clausify: Middleware<SlackEventMiddlewareArgs<'message'>> = async ({ context, next }) => {
+        context.clauses = context.matches[1].trim().split(re_semi);
+
+        await next?.();
     };
-    app.message(listen_roll, anywhere, async ({ message, context, client, say }) => {
-        try {
-            context.clauses = await macroize(store, context, message.user);
 
-            const clauses = context.clauses.length;
-            for (let i = 0; i < clauses; i++)
-                context.clauses.push(...expandRepeats(context.clauses.shift()));
-
-            const results = rollDice(context.clauses, message);
-
-            for (const where in results) {
-                if (results[where].blocks.length > 0)
-                    await say({
-                        token: context.botToken,
-                        channel: message.channel,
-                        username: 'Roll',
-                        icon_emoji: ':game_die:',
-                        text: `<@${message.user}> rolled dice`,
-                        blocks: results[where].blocks,
-                        ...(message.thread_ts ? {
-                            thread_ts: message.thread_ts
-                        } : where == 'thread' ? {
-                            thread_ts: message.ts
-                        } : {})
-                    });
-            }
-        }
-        catch (err) {
-            await blame({ error: err, message, context, client });
-        }
-    });
-
-    async function macroize(store: Promise<MongoClient>, context: Context, user: string) {
+    const macrotize: Middleware<SlackEventMiddlewareArgs<'message'>> = async ({ message, context, next }) => {
         const coll = (await store).db().collection('macros');
         const personal = (await coll.findOne(
-                { _id: user },
+                { _id: message.user },
                 { projection: { _id: 0} }
             )) ?? {},
             community = (await coll.findOne(
@@ -84,19 +32,43 @@ export const events = (app: App, store: Promise<MongoClient>): void => {
 
         if (Object.keys(macros).length >= 1) {
             const re_macros = new RegExp(`\\b(${Object.keys(macros).join('|')})\\b`, 'gi');
-            context.clauses = context.clauses.map((clause: Clause) => ({
-                text: clause.text.replace(re_macros, m => macros[m.toLowerCase()]),
-                where: clause.where
-            }));
+            context.clauses = context.clauses.map((clause: string) =>
+                clause.replace(re_macros, m => macros[m.toLowerCase()])
+            );
         }
 
-        return context.clauses;
-    }
+        await next?.();
+    };
+
+    const re_roll = /^!?roll\s+(.+)/is;
+    app.message(re_roll, anywhere, clausify, macrotize, async ({ message, context, client, say }) => {
+        try {
+            const clauses = context.clauses.length;
+            for (let i = 0; i < clauses; i++)
+                context.clauses.push(...expandRepeats(context.clauses.shift()));
+
+            const results = rollDice(context.clauses, message);
+
+            if (results.blocks.length > 0)
+                await say({
+                    token: context.botToken,
+                    channel: message.channel,
+                    username: 'Roll',
+                    icon_emoji: ':game_die:',
+                    text: `<@${message.user}> rolled dice`,
+                    blocks: results.blocks,
+                    thread_ts: message.thread_ts ?? message.ts
+                });
+        }
+        catch (err) {
+            await blame({ error: err, message, context, client });
+        }
+    });
 
     const re_ellipsis = /^\s*(.+)\s*\.\.\.\s*(\w+(?:\s*,\s*\w+)*)\s*$/,
         re_commas = /\s*,\s*/;
-    function expandRepeats(clause: Clause) {
-        const match = re_ellipsis.exec(clause.text);
+    function expandRepeats(clause: string) {
+        const match = re_ellipsis.exec(clause);
         if (match) {
             const phrase = match[1].replace(re_trail, ''),
                 over = match[2].split(re_commas),
@@ -104,16 +76,10 @@ export const events = (app: App, store: Promise<MongoClient>): void => {
             const clones = [];
             if (over.length == 1 && reps >= 1)
                 for (let i = 1; i <= reps; i++)
-                    clones.push({
-                        text: `${phrase} on the ${ordinal(i)} roll`,
-                        where: clause.where
-                    });
+                    clones.push(`${phrase} on the ${ordinal(i)} roll`);
             else
                 for (let i = 0; i < over.length; i++)
-                    clones.push({
-                        text: `${phrase} for ${over[i]}`,
-                        where: clause.where
-                    });
+                    clones.push(`${phrase} for ${over[i]}`);
             return clones;
         }
         else {
@@ -121,22 +87,14 @@ export const events = (app: App, store: Promise<MongoClient>): void => {
         }
     }
 
-    function rollDice(clauses: Clause[], message: MessageEvent) {
+    function rollDice(clauses: string[], message: MessageEvent) {
         const results: {
-            [where: string]: {
-                text?: string,
-                phrases: string[],
-                blocks: Block[];
-            }
+            text?: string,
+            phrases: string[],
+            blocks: Block[];
         } = {
-            inline: {
-                phrases: [],
-                blocks: []
-            },
-            thread: {
-                phrases: [],
-                blocks: []
-            }
+            phrases: [],
+            blocks: []
         };
 
         for (let i = 0; i < clauses.length; i++) {
@@ -145,7 +103,7 @@ export const events = (app: App, store: Promise<MongoClient>): void => {
             const elements = [];
 
             const re_dice_code = /\b([1-9][0-9]*)?d([1-9][0-9]*|%)(?:([HL])([1-9][0-9]*)?)?([+-][0-9]+(?:\.[0-9]+)?)?\b/ig;
-            const outcome = clauses[i].text.replace(re_dice_code, (expr, count, size, hilo, keep, mod) => {
+            const outcome = clauses[i].replace(re_dice_code, (expr, count, size, hilo, keep, mod) => {
                 count = parseInt(count) || 1;
                 size = (size != '%' ? parseInt(size) || 1 : 100);
 
@@ -225,32 +183,32 @@ export const events = (app: App, store: Promise<MongoClient>): void => {
                     text: `:warning: Too many context elements to show (limit of ${MAX_CONTEXT_ELEMENTS}).`
                 };
 
-            if (outcome != clauses[i].text) {
-                clauses[i].text = outcome;
+            if (outcome != clauses[i]) {
+                clauses[i] = outcome;
                 clauses[i] = evaluateArithmetic(clauses[i]);
                 clauses[i] = prettifyMarkdown(clauses[i]);
-                results[clauses[i].where].phrases.push(clauses[i].text);
+                results.phrases.push(clauses[i]);
 
-                if (results[clauses[i].where].blocks.length < MAX_MESSAGE_BLOCKS) {
-                    if (results[clauses[i].where].blocks.length == 0)
-                        results[clauses[i].where].blocks.push(<SectionBlock>{
+                if (results.blocks.length < MAX_MESSAGE_BLOCKS) {
+                    if (results.blocks.length == 0)
+                        results.blocks.push(<SectionBlock>{
                             type: 'section',
                             text: {
                                 type: 'mrkdwn',
-                                text: trunc(`<@${message.user}> rolled ${clauses[i].text}.`, MAX_TEXT_SIZE)
+                                text: trunc(`<@${message.user}> rolled ${clauses[i]}.`, MAX_TEXT_SIZE)
                             }
                         });
                     else
-                        results[clauses[i].where].blocks.push(<SectionBlock>{
+                        results.blocks.push(<SectionBlock>{
                             type: 'section',
                             text: {
                                 type: 'mrkdwn',
-                                text: trunc(`Then ${clauses[i].text}.`, MAX_TEXT_SIZE)
+                                text: trunc(`Then ${clauses[i]}.`, MAX_TEXT_SIZE)
                             }
                         });
 
                     if (elements.length > 0)
-                        results[clauses[i].where].blocks.push(<ContextBlock>{
+                        results.blocks.push(<ContextBlock>{
                             type: 'context',
                             elements: elements
                         });
@@ -258,27 +216,26 @@ export const events = (app: App, store: Promise<MongoClient>): void => {
             }
         }
 
-        for (const where in results) {
-            if (results[where].blocks.length == MAX_MESSAGE_BLOCKS)
-                results[where].blocks[MAX_MESSAGE_BLOCKS-1] = <SectionBlock>{
-                    type: 'section',
-                    text: {
-                        type: 'mrkdwn',
-                        text: `:warning: Too many message blocks to show (limit of ${MAX_MESSAGE_BLOCKS}).`
-                    }
-                };
+        if (results.blocks.length == MAX_MESSAGE_BLOCKS)
+            results.blocks[MAX_MESSAGE_BLOCKS-1] = <SectionBlock>{
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: `:warning: Too many message blocks to show (limit of ${MAX_MESSAGE_BLOCKS}).`
+                }
+            };
 
-            if (results[where].phrases.length > 0)
-                results[where].text = trunc(results[where].phrases.join('; '), MAX_TEXT_SIZE);
-        }
+        if (results.phrases.length > 0)
+            results.text = trunc(results.phrases.join('; '), MAX_TEXT_SIZE);
+
         return results;
     }
 
     const re_number = /\b(?<![:_~*])[1-9][0-9]*(?![:_~*])\b/g,
         re_tag = /<([^>]+)>/g,
         re_trail = /^[\s.;,]+|[\s.;,]+$/g;
-    function prettifyMarkdown(clause: Clause) {
-        clause.text = wss(clause.text
+    function prettifyMarkdown(clause: string) {
+        clause = wss(clause
             .replace(re_number, '*$&*')
             .replace(re_tag, '')
             .replace(re_trail, '')
@@ -287,7 +244,7 @@ export const events = (app: App, store: Promise<MongoClient>): void => {
     }
 
     const re_math = /([+-]|\b)([0-9]+(?:\.[0-9]+)?)\s*([+-])\s*([0-9]+(?:\.[0-9]+)?)\b/;
-    function evaluateArithmetic(clause: Clause) {
+    function evaluateArithmetic(clause: string) {
         return regexClosure(clause, re_math, (_, sign, x, op, y) => {
             const ix = parseInt(`${sign}${x}`),
                 iy = parseInt(`${op}${y}`),
@@ -298,12 +255,12 @@ export const events = (app: App, store: Promise<MongoClient>): void => {
         });
     }
 
-    function regexClosure(clause: Clause, re: RegExp, fun: (substring: string, ...args: (string | number)[]) => string) {
+    function regexClosure(clause: string, re: RegExp, fun: (substring: string, ...args: (string | number)[]) => string) {
         let old;
         do {
-            old = clause.text;
-            clause.text = clause.text.replace(re, fun);
-        } while (clause.text != old);
+            old = clause;
+            clause = clause.replace(re, fun);
+        } while (clause != old);
         return clause;
     }
 };
