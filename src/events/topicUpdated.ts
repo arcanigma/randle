769a157fc5f57b2dbf4c54aa8853fc80bcaa601f
Ron Reaser <ron@reaser.org>
2021-08-22 20:56:24 -0400

    No audit logs for topic update attribution

--- src/events/topicUpdated.ts
+++ src/events/topicUpdated.ts
@@ -1,19 +1,12 @@
-import { Client, TextChannel, User } from 'discord.js';
+import { Client, TextChannel } from 'discord.js';
 
 export const register = ({ client }: { client: Client }): void => {
 
     client.on('channelUpdate', async (oldChannel, newChannel) => {
         if (oldChannel instanceof TextChannel && newChannel instanceof TextChannel) {
-            const log = (await newChannel.guild.fetchAuditLogs({
-                limit: 1,
-                type: 'CHANNEL_UPDATE',
-            })).entries.first();
-
             if (newChannel.topic && newChannel.topic != oldChannel.topic) {
                 await newChannel.send({
-                    content: log?.executor instanceof User
-                        ? `_${log.executor.toString()} set the channel topic:_ ${newChannel.topic}`
-                        : `_New channel topic:_ ${newChannel.topic}`
+                    content: `_New channel topic:_ ${newChannel.topic}`
                 });
             }
         }
