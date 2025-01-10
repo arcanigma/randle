6caf9d57ddfc9f23aa31f029e37b06dfee69dbbb
Ron Reaser <ron@reaser.org>
2025-01-09 23:54:04 -0500

    Streamlined parsing of topic updates

--- src/events/channelUpdate.ts
+++ src/events/channelUpdate.ts
@@ -1,34 +1,33 @@
 import { Channel, Colors, Events, TextChannel } from 'discord.js';
 import * as inflection from 'inflection';
-import * as emoji from 'node-emoji';
 import { MAX_EMBED_DESCRIPTION, MAX_EMBED_FIELDS, MAX_EMBED_TITLE, MAX_FIELD_NAME, MAX_FIELD_VALUE } from '../library/constants.js';
 import { trunc } from '../library/texts.js';
 
 export const name = Events.ChannelUpdate;
 export const once = false;
 
-const re_colon = /(?:(.+?):)?(.+)/;
+const re_bracketing = /^\s*(?:\[\s*([^\]]+?)\s*\]\s*)?(.+?)\s*$/;
 export async function execute (oldChannel: Channel, newChannel: Channel): Promise<void> {
     if (oldChannel instanceof TextChannel && newChannel instanceof TextChannel) {
         if (newChannel.topic && newChannel.topic != oldChannel.topic) {
             if (newChannel.topic.includes('|')) {
-                const [ first, ...rest ] = emoji.emojify(newChannel.topic).split('|').map(it => it.trim()).filter(Boolean).slice(0, MAX_EMBED_FIELDS+1);
+                const [ first, ...rest ] = newChannel.topic.split('|').map(it => it.trim()).filter(Boolean).slice(0, MAX_EMBED_FIELDS+1);
 
-                const [ , title, description ] = first.match(re_colon) as string[];
+                const [ , title, description ] = first.match(re_bracketing) as string[];
 
                 await newChannel.send({
                     content: '_New channel topic:_',
                     embeds: [
                         {
-                            title: title ? trunc(emoji.unemojify(title), MAX_EMBED_TITLE) : 'Topic',
-                            description: trunc(emoji.unemojify(description), MAX_EMBED_DESCRIPTION),
+                            title: title ? trunc(title, MAX_EMBED_TITLE) : 'Topic',
+                            description: trunc(description, MAX_EMBED_DESCRIPTION),
                             color: Colors.Blurple,
                             fields: rest.map((it, index) => {
-                                const [ , name, value ] = it.match(re_colon) as string[];
+                                const [ , name, value ] = it.match(re_bracketing) as string[];
 
                                 return {
-                                    name: name ? trunc(emoji.unemojify(name), MAX_FIELD_NAME) : inflection.ordinalize(`${index+1} Subtopic`),
-                                    value: trunc(emoji.unemojify(value), MAX_FIELD_VALUE),
+                                    name: name ? trunc(name, MAX_FIELD_NAME) : inflection.ordinalize(`${index+1} Subtopic`),
+                                    value: trunc(value, MAX_FIELD_VALUE),
                                     inline: true
                                 };
                             })
